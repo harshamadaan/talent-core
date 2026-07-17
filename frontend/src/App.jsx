34a97f8b292b4558753ee74ac4916d4f99c3bcf7
Harsha Arora <harshamadaan50@gmail.com>
2026-07-18 02:44:56 +0530

    resolve clerk integeration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+import "./App.css";
 
 function App() {
  
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import App from './App.jsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if(!PUBLISH_KEY){
+if(!PUBLISHABLE_KEY){
   throw new Error("Missing Clerk Publishable Key");
 }
 
@@ -18,7 +18,9 @@ createRoot(document.getElementById('root')).render(
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
       afterSignOutUrl="/"
-    ></ClerkProvider>
-    <App />
+    >
+       <App />
+    </ClerkProvider>
+   
   </StrictMode>,
 )
